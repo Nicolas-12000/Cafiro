a9e1c8ae88e9514ed309466eac8337a470a2aab6
nicolas <nikolasg12000@gmail.com>
2025-12-30 18:11:31 -0500

    first commit

---
+++ cafiro/src/main/java/com/nicolasgarcia/cafiro/CafiroApplication.java
@@ -0,0 +1,13 @@
+package com.nicolasgarcia.cafiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafiroApplication.class, args);
+	}
+
+}
---
+++ cafiro/src/test/java/com/nicolasgarcia/cafiro/CafiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasgarcia.cafiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
